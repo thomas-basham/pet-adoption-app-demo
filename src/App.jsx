5de7ee7f3de5154597f3eee7e93f7c3fb74e2d7c
Thomas Basham <Bashamtg@gmail.com>
2025-09-02 16:49:44 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <header>
+    <h1>
+      March Cohort's Pet Adoptions
+    </h1>
+   </header>
+   <main></main>
+   <footer></footer>
+    </>
+  )
+}
+
+export default App
