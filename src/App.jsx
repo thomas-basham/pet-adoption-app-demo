dcabfc71217a76f00350d5fe07d8cdb426baf6e3
Thomas Basham <Bashamtg@gmail.com>
2025-09-04 17:33:07 -0700

    added update, delete handlers, addded modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useState, useEffect } from "react";
 import "./App.scss";
-import { createItem, listAllItems } from "./utils/dynamo";
+import {
+  createItem,
+  listAllItems,
+  updateItem,
+  deleteItem,
+} from "./utils/dynamo";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 function App() {
   // id: String,  name: string, age: number, isAdopted: boolean
   const [pets, setPets] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [petToUpdate, setPetToUpdate] = useState({});
 
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
   useEffect(() => {
     (async () => {
       const items = await listAllItems("Pet");
@@ -13,6 +35,12 @@ function App() {
       setPets(items);
     })();
   }, []);
+
+  const handleOpen = (petObject) => {
+    setPetToUpdate(petObject);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   const handleAddPet = async (event) => {
     event.preventDefault();
@@ -33,6 +61,37 @@ function App() {
     });
   };
 
+  const handleUpdatePet = async (event) => {
+    event.preventDefault();
+
+    const { age, isAdopted } = petToUpdate;
+
+    console.log(petToUpdate.id);
+    await updateItem(
+      "Pet",
+      { id: petToUpdate.id, name: petToUpdate.name },
+      { age, isAdopted }
+    );
+
+    setPets((oldPets) => {
+      return oldPets.map((petObject) => {
+        return petObject.id === petToUpdate.id ? petToUpdate : petObject;
+      });
+    });
+
+    setOpen(false);
+  };
+
+  const handleDeletePet = async (id, name) => {
+    await deleteItem("Pet", { id: id, name: name });
+    console.log(id);
+    setPets((oldPets) => {
+      return oldPets.filter((petObject) => {
+        return petObject.id == id;
+      });
+    });
+  };
+
   return (
     <>
       <header>
@@ -41,13 +100,13 @@ function App() {
       <main>
         <form onSubmit={(event) => handleAddPet(event)}>
           <h2>Pet Intake Form</h2>
-          <label htmlFor="">Name</label>
+          <label htmlFor="petName">Name</label>
           <input type="text" name="petName" id="petName" />
           <br />
-          <label htmlFor="">Age</label>
+          <label htmlFor="age">Age</label>
           <input type="number" name="age" id="age" />
           <br />
-          <label htmlFor="">Adopted</label>
+          <label htmlFor="isAdopted">Adopted</label>
           <input type="checkbox" name="isAdopted" id="isAdopted" />
           <br />
           <button type="submit">Add Pet</button>
@@ -66,11 +125,86 @@ function App() {
                       <p>{petObject.name}</p>
                       <p>{petObject.age}</p>
                       <p>{petObject.isAdopted ? "Adopted" : "Needs a Home"}</p>
+                      <Button onClick={() => handleOpen(petObject)}>
+                        Update Pet Information
+                      </Button>
+                      <Button
+                        onClick={() =>
+                          handleDeletePet(petObject.id, petObject.name)
+                        }
+                      >
+                        Remove From Inventory
+                      </Button>
                     </div>
                   );
                 })}
             </div>
           )}
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <form onSubmit={(event) => handleUpdatePet(event)}>
+                <h2>Pet Intake Form</h2>
+                <label htmlFor="petName">Name</label>
+                <input
+                  value={petToUpdate.name}
+                  type="text"
+                  name="petName"
+                  id="petName"
+                  disabled
+                  onChange={(event) =>
+                    setPetToUpdate({
+                      id: petToUpdate.id,
+                      name: event.target.value,
+                      age: petToUpdate.age,
+                      isAdopted: petToUpdate.isAdopted,
+                    })
+                  }
+                />
+                <br />
+                <label htmlFor="age">Age</label>
+                <input
+                  onChange={(event) =>
+                    setPetToUpdate({
+                      id: petToUpdate.id,
+                      name: petToUpdate.name,
+                      age: event.target.value,
+                      isAdopted: petToUpdate.isAdopted,
+                    })
+                  }
+                  value={petToUpdate.age}
+                  type="number"
+                  name="age"
+                  id="age"
+                />
+                <br />
+                <label htmlFor="isAdopted">Adopted</label>
+                <input
+                  onChange={(event) =>
+                    setPetToUpdate({
+                      id: petToUpdate.id,
+                      name: petToUpdate.name,
+                      age: petToUpdate.age,
+                      isAdopted: event.target.checked,
+                    })
+                  }
+                  checked={petToUpdate.isAdopted}
+                  type="checkbox"
+                  name="isAdopted"
+                  id="isAdopted"
+                />
+                <br />
+                <button type="submit">Add Pet</button>
+              </form>
+            </Box>
+          </Modal>
         </section>
       </main>
       <footer></footer>
